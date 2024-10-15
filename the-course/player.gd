6cb9ad44aa1ce71be0ecabc116aada9922745879
mcsurvivalmode <C24385051@mytudublin.ie>
2024-10-15 17:39:28 +0100

    bullet in space shooter

--- the-course/player.gd
+++ the-course/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @export var bullet_scene:PackedScene
-@export var bullet_spawn_point:Node2D
+@export var bullet_spawn_point:Node2D #not workinh bullets bleh
 
 const SPEED = 500.0
 const JUMP_VELOCITY = -400.0
@@ -22,7 +22,7 @@ func _physics_process(delta: float) -> void:
 	print(transform.y)
 	velocity = vel
 	
-	if Input.is_action_pressed("fire"):
+	if Input.is_action_pressed("fire"): #bullet code
 		var b = bullet_scene.instantiate()
 		b.global_position = bullet_spawn_point.global_position
 		get_parent().add_child(b)

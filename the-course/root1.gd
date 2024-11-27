868a126e9d8d20a52c5c25d43d022fec02227d64
mcsurvivalmode <C24385051@mytudublin.ie>
2024-11-27 14:57:45 +0000

    comments from class

--- the-course/root1.gd
+++ the-course/root1.gd
@@ -7,7 +7,7 @@ func _ready():
 	# DebugDraw2D.config.text_default_size = 24
 	pass
 	
-func _input(event):
+func _input(event): #commment comment 
 	if event is InputEventKey:
 		var key_event = event as InputEventKey
 		if key_event.keycode == KEY_Q and key_event.pressed:

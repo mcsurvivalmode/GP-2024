0a203bf6b1ae7db9c0ebc6df146500e0c021d887
mcsurvivalmode <C24385051@mytudublin.ie>
2025-02-06 15:45:01 +0000

    end of class- not working :[

--- term 2/week2_flowers/gen_art.gd
+++ term 2/week2_flowers/gen_art.gd
@@ -2,6 +2,7 @@ extends Node2D
 var radius:float 
 var count:int
 var color_hue_start
+var middle:float
 	
 	
 	
@@ -12,9 +13,7 @@ func reset() -> void:
 	color_hue_start = 0
 
 
-
-
-func _draw() ->void:
+func _draw() -> void:
 	var l = float(count)
 	#for i in range(l):
 		#draw_line(Vector2(i * 100, 0), Vector2(i * 100, 500), Color.AQUAMARINE, 20)
@@ -26,27 +25,28 @@ func _draw() ->void:
 	var color_hue = color_hue_start 
 	var alpha = $"../ColorPickerButton".color.a
 	
+
+	
 	for i in range(l): #creates the cirlces by calculating the spaceee
 		var theta = theta_inc * i 
 		var x = sin(theta) * r
 		var y = cos(theta) * r
 		var c:Color = Color.from_hsv(color_hue, 1, 1, alpha)
 		draw_line(Vector2(px, py), Vector2(x,y), c, 10)
-		#draw_circle(Vector2(x,y), 10, Color.PINK, false)
+		#draw_circle(Vector2(x,y), radius, Color.PINK, false)
 		px = x
 		py = y
 		r = r + radius
 	
+	#var c_2:Color = Color.from_hsv(color_hue_2, 1, 1, alpha_2 )
+	draw_circle(Vector2(0,0), radius, Color.PINK, true)
+	
+
 	pass
 	
 
 	
-func _process(delta: float) -> void:
-	queue_redraw()    
-	rotation = lerp(rotation, Input.get_axis("forward", "back") * PI, 
-	delta)
-	# global_position.x += delta * 100
-	pass
+
 
 
 func _on_num_lines_value_changed(value: float) -> void:
@@ -61,4 +61,9 @@ func _on_radius_value_changed(value: float) -> void:
 
 func _on_color_picker_button_color_changed(color: Color) -> void:
 	color_hue_start = color.h
+	pass # Replace with function body.
+
+
+func _on_middle_value_changed(value: float) -> void:
+	middle = value
 	pass # Replace with function body.

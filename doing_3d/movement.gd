95f971fc62db6046fef5eaaeaefcb3ca774ddb81
mcsurvivalmode <C24385051@mytudublin.ie>
2025-03-16 23:26:42 +0000

    runner start

---
+++ doing_3d/movement.gd
@@ -0,0 +1,7 @@
+extends CharacterBody3D
+
+@export var speed: float = 10.0
+@export var gravity: float = -24.8
+@export var jump_force: float = 10.0 
+
+@export var velocity: Vector3 = Vector3.ZERO 
